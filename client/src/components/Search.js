b64a9230c59e23d2ce8f988d7f1e73675da72440
Moksh <mokshsharma1110@gmail.com>
2024-07-26 15:14:15 +0530

    nw

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -11,7 +11,7 @@ export const Search = () => {
     const clear = () => {
         setInput("");
         setBestMatch([]);
-    }
+    }   
     const UpdateBestMatch = () => {
         setBestMatch(mockSearchResult.result);
     }
